0e8af71a70e5e91fcb1a1c382ce97a2eb07f2c9e
melsonj <jmelson@everettcc.edu>
2019-05-09 19:59:04 -0700

    Cookie

--- app.js
+++ app.js
@@ -126,8 +126,6 @@ app.get('/login', async function(req, res, next) {
     
     console.log('Login route ...', Date.now());
     
-    let 
-    
     // No successful LTI launch
     if (ltiDetails === null) {
         res.status(403).send('ERROR: This page can only be accessed following a valid LTI launch.');
