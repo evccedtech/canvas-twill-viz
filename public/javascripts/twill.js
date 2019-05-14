cc2e3ff1f8ffdcf6d3da0b8df4412b938a1cf6de
melsonj <jmelson@everettcc.edu>
2019-05-14 16:26:27 -0700

    Viz width calculation

--- public/javascripts/twill.js
+++ public/javascripts/twill.js
@@ -91,7 +91,7 @@ function getAuthorInfo(authorInfo) {
 }
 
 function getVizWidth() {
-    return $('#wrapper #viz').width();
+    return Math.floor($('#wrapper #viz').innerWidth());
 }
 
 function sumMessageLength(node) {
