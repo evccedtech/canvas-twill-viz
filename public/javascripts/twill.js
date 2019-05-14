07d929a4c28378ac2daef2c71534e184b87136b4
melsonj <jmelson@everettcc.edu>
2019-05-14 16:46:35 -0700

    Page layout

--- public/javascripts/twill.js
+++ public/javascripts/twill.js
@@ -91,7 +91,7 @@ function getAuthorInfo(authorInfo) {
 }
 
 function getVizWidth() {
-    return Math.floor($('#wrapper #viz').innerWidth() * .8);
+    return Math.floor($('#wrapper #viz').innerWidth());
 }
 
 function sumMessageLength(node) {
